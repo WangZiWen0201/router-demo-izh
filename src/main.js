f1a84a5b8b9daeea6eafd6af7eddb0f11ba5853a
osmond <234501354@qq.com>
2017-09-13 11:27:24 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import router from './router'
-import MintUI from 'mint-ui';
-import 'mint-ui/lib/style.css';
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
 
 Vue.use(MintUI);
 
